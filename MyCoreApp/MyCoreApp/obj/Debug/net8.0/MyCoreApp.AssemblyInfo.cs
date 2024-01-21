d31712885a1eccca1e6a83293624b9d47ad8f773
Amanda Ike <amonte13@my.centennialcollege.ca>
2024-01-21 10:54:41 -0500

    changed index page

--- MyCoreApp/MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc51f59fbfb0bc3324ab6fa926c6b548f2c1dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
